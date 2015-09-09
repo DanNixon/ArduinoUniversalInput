dd0f24665b1b6b81eebeb95f61fdb670b0e09f46
Dan Nixon <dan@dan-nixon.com>
2015-09-09 21:50:58 +0100

    FIx build

--- UniversalInputTypes.h
+++ UniversalInputTypes.h
@@ -1,13 +1,10 @@
 #ifndef _ARDUINOUNIVERSALINPUTS_TYPES_H
 #define _ARDUINOUNIVERSALINPUTS_TYPES_H
 
+#include <Arduino.h>
 #include <inttypes.h>
 
 #define NULL 0
-
-#ifdef __AVR__
-typedef uint16_t size_t;
-#endif
 
 typedef uint16_t inputid_t;
 typedef uint8_t inputpin_t;
